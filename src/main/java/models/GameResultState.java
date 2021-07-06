29da43b452b6afba12ba0b8eabf0c8533a216c22
Nikita Berkutov <nick9733@gmail.com>
2021-07-06 12:44:28 +0600

    Game, GameResult, SelfPlay

---
+++ src/main/java/models/GameResultState.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum GameResultState {
+    NONE,
+    BLACK,
+    WHITE,
+    DRAW,
+}
--- src/main/java/services/SelfPlay.java
+++ src/main/java/services/SelfPlay.java
@@ -1,7 +1,26 @@
 package services;
 
+import models.Board;
+import models.Game;
+import models.GameResult;
+import models.Player;
+
 public class SelfPlay {
+    private final Player first;
+    private final Player second;
+    private final Game game;
 
+    public SelfPlay(Player first, Player second) {
+        this.first = first;
+        this.second = second;
+        game = new Game(first, second);
+    }
 
-
+    public void play() {
+        while (!game.isFinished()) {
+            game.next();
+            Board board = game.getBoard();
+        }
+        GameResult result = game.getResult();
+    }
 }
