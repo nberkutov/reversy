e2ac6f91f6846f09c3cc4aa111e95309dd5c257c
VyacheslavSuper <slavik-klas.super@yandex.ru>
2021-07-06 12:48:07 +0600

    Dell sets in GameBoard

--- src/main/java/models/Board.java
+++ src/main/java/models/Board.java
@@ -166,11 +166,6 @@ public class Board implements GameBoard {
     }
 
     @Override
-    public Set<Point> getAvailableMoves(PlayerColor color) {
-        return null;
-    }
-
-    @Override
     public void reverseCell(int x, int y) throws GameException {
         reverseCell(new Point(x, y));
     }
