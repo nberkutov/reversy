8afd1fd10b96cb3d635c546bfaaddc6f6a7a557d
Nikita Berkutov <nick9733@gmail.com>
2021-07-13 17:37:57 +0600

    rename test

--- src/test/java/models/Bot10KGamesTest.java
+++ src/test/java/models/Bot10KGamesTest.java
@@ -8,13 +8,14 @@ import services.SelfPlay;
 class Bot10KGamesTest {
 
     @Test
-    public void testGame() throws GameException, InterruptedException {
+    public void testGame() throws GameException {
         RandomBot bot1 = new RandomBot(0);
         RandomBot bot2 = new RandomBot(1);
         SelfPlay selfPlay = new SelfPlay(bot1,bot2);
         selfPlay.play();
     }
 
+    @Test
     void test10kGames() throws GameException {
         for (int i = 3000; i < 6000; i++) {
             RandomBot bot1 = new RandomBot(0);
