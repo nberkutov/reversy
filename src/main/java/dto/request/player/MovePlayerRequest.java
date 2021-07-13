ae456cc91174967ec64bf17fa3765d3573837dcf
Fuper <Super4121@gmail.com>
2021-07-13 17:44:31 +0600

    Fix request

--- src/main/java/dto/request/player/MovePlayerRequest.java
+++ src/main/java/dto/request/player/MovePlayerRequest.java
@@ -1,19 +1,15 @@
 package dto.request.player;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import models.board.Point;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MovePlayerRequest extends GameRequest {
     private int playerId;
     private int gameId;
     private Point point;
-
-    public MovePlayerRequest(int playerId, int gameId, Point point) {
-        this.playerId = playerId;
-        this.gameId = gameId;
-        this.point = point;
-    }
 }
