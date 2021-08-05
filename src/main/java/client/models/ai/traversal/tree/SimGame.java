10008b773507271430697233967d44b8994ceb71
VyacheslavSuper <slavik-klas.super@yandex.ru>
2021-08-05 11:25:21 +0600

    Traversal

---
+++ src/main/java/client/models/ai/traversal/tree/SimGame.java
@@ -0,0 +1,33 @@
+package client.models.ai.traversal.tree;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import models.base.PlayerColor;
+import models.base.interfaces.GameBoard;
+import models.board.Point;
+
+@Data
+@AllArgsConstructor
+public class SimGame {
+    private GameBoard board;
+    private Point move;
+    private PlayerColor colorMove;
+    private int score;
+    private boolean gameEnd;
+
+    public SimGame(GameBoard board, PlayerColor colorMove) {
+        this.board = board;
+        this.colorMove = colorMove;
+        gameEnd = false;
+    }
+
+    @Override
+    public String toString() {
+        return "SimGame{" +
+                "move=" + move +
+                ", colorMove=" + colorMove +
+                ", score=" + score +
+                ", gameEnd=" + gameEnd +
+                '}';
+    }
+}
