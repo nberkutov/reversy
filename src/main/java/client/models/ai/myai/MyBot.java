c9ce7dab8b05c1d345fd069da15fb6ba3f181d8d
Fuper <Super4121@gmail.com>
2021-08-06 12:18:25 +0600

    Fix MyBot

--- src/main/java/client/models/ai/myai/MyBot.java
+++ src/main/java/client/models/ai/myai/MyBot.java
@@ -1,12 +1,16 @@
 package client.models.ai.myai;
 
 import client.models.ai.minimax.tree.Tree;
+import client.models.strategies.Strategy;
 import exception.GameException;
 import models.base.PlayerColor;
 import models.base.interfaces.GameBoard;
 import models.board.Point;
 
 public class MyBot {
+
+    public MyBot(int maxDeath, PlayerColor color, Strategy strategyPlayer) {
+    }
 
     public int calculateMove(final Tree branch, final GameBoard board, Point move, int depth, PlayerColor moveColor) throws GameException {
         return 0;
