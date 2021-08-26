ead72177264c67ea94104827407e0f6dfe8ee6bc
nberkutov <nick9733@gmail.com>
2021-08-26 11:37:55 +0600

    Update MinimaxStrategyTest.java

--- client/src/test/java/strategy/MinimaxStrategyTest.java
+++ client/src/test/java/strategy/MinimaxStrategyTest.java
@@ -18,14 +18,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MinimaxStrategyTest {
 
     @Test
-    void testMinimax() {
-        final Player minimaxPlayer = new BotPlayer("minimax", new MinimaxStrategy(3, Utility::advanced));
+    void testMinimaxWhiteDepth3() {
+        final Player minimaxPlayer = new BotPlayer("minimax", new MinimaxStrategy(3, Utility::multiHeuristic));
         final Player randomBot = new BotPlayer("random", new RandomStrategy());
 
         minimaxPlayer.setColor(PlayerColor.WHITE);
         randomBot.setColor(PlayerColor.BLACK);
 
         final GameBoard board = new ArrayBoard("wwwwwwwbwbwwbwwwwwbwwbwbwwbwbwwbwwwbwwwbwwbbbwwewbwwwbwebbwbbbbe");
+
         try {
             Point move = minimaxPlayer.move(board);
             BoardLogic.makeMove(board, move, Cell.valueOf(minimaxPlayer.getColor()));
@@ -37,6 +38,32 @@ class MinimaxStrategyTest {
             BoardLogic.makeMove(board, move, Cell.valueOf(minimaxPlayer.getColor()));
 
             assertTrue(board.getCountWhiteCells() > board.getCountBlackCells());
+        } catch (final ServerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testMinimaxBlackDepth3() {
+        final Player minimaxPlayer = new BotPlayer("minimax", new MinimaxStrategy(3, Utility::multiHeuristic));
+        final Player randomBot = new BotPlayer("random", new RandomStrategy());
+
+        minimaxPlayer.setColor(PlayerColor.BLACK);
+        randomBot.setColor(PlayerColor.WHITE);
+
+        final GameBoard board = new ArrayBoard("bbwwewbbwbwwwwbbewbbbwbewbwwwwbwbbbwbwbwwbwbwwbwwbbbbwwbwbewewwe");
+
+        try {
+            Point move = minimaxPlayer.move(board);
+            BoardLogic.makeMove(board, move, Cell.valueOf(minimaxPlayer.getColor()));
+
+            move = randomBot.move(board);
+            BoardLogic.makeMove(board, move, Cell.valueOf(randomBot.getColor()));
+
+            move = minimaxPlayer.move(board);
+            BoardLogic.makeMove(board, move, Cell.valueOf(minimaxPlayer.getColor()));
+
+            assertTrue(board.getCountBlackCells() > board.getCountWhiteCells());
         } catch (final ServerException e) {
             e.printStackTrace();
         }
