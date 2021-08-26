b729892074449e99ff4c4ced31fc2b156da6426d
nberkutov <nick9733@gmail.com>
2021-08-26 11:26:41 +0600

    Update ABPruningStrategy.java

--- client/src/main/java/strategy/ABPruningStrategy.java
+++ client/src/main/java/strategy/ABPruningStrategy.java
@@ -25,7 +25,7 @@ public class ABPruningStrategy implements Strategy {
     public Point move(final GameBoard board) throws ServerException {
         final List<Point> moves = ClientBoardLogic.getAvailableMoves(board, color);
         double maxWin = Integer.MIN_VALUE;
-        Point maxMove = null;
+        Point maxMove = moves.get(0);
         for (final Point move : moves) {
             final GameBoard boardCopy = new ArrayBoard(board);
             ClientBoardLogic.makeMove(boardCopy, move, Cell.valueOf(color));
@@ -34,37 +34,31 @@ public class ABPruningStrategy implements Strategy {
                 maxWin = win;
                 maxMove = move;
             }
-        }
-        if (maxMove == null) {
-            return moves.get(0);
-        }
+        }      
         return maxMove;
     }
 
     private double minimax(final GameBoard board, final int depth, final PlayerColor currentColor, double alpha, double beta)
             throws ServerException {
-        final PlayerColor simColor;
         final ToDoubleBiFunction<GameBoard, PlayerColor> estimateFunc;
         final boolean maximizingPlayer;
         if (currentColor == color) {
-            simColor = color;
             estimateFunc =  utility;
             maximizingPlayer = true;
-        } else {
-            simColor = revert(color);
+        } else {            
             estimateFunc = (b, c) -> -utility.applyAsDouble(b, c);
             maximizingPlayer = false;
         }
 
         final PlayerColor winner = getEndOfGame(board);
         if (depth == 0 || winner != PlayerColor.NONE) {
-            return estimateFunc.applyAsDouble(board, simColor);
+            return estimateFunc.applyAsDouble(board, currentColor);
         }
-        final List<Point> availableMoves = ClientBoardLogic.getAvailableMoves(board, simColor);
+        final List<Point> availableMoves = ClientBoardLogic.getAvailableMoves(board, currentColor);
         double maxWin = Integer.MIN_VALUE;
         for (final Point move : availableMoves) {
             final GameBoard copy = new ArrayBoard(board);
-            ClientBoardLogic.makeMove(copy, move, Cell.valueOf(simColor));
+            ClientBoardLogic.makeMove(copy, move, Cell.valueOf(currentColor));
             final double win = minimax(copy, depth - 1, revert(currentColor), alpha, beta);
             if (maximizingPlayer) {
                 if (win > beta) {
@@ -80,11 +74,6 @@ public class ABPruningStrategy implements Strategy {
             if (win > maxWin) {
                 maxWin = win;
             }
-            /*if (maximizingPlayer) {
-                alpha = Math.max(win, alpha);
-            } else {
-                beta = Math.min(win, beta);
-            }*/
         }
         return maxWin;
     }
